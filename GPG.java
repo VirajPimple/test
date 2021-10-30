cc334f6384016f84ae8ac20187bf359e9572272f
VirajPimple <87273217+VirajPimple@users.noreply.github.com>
2021-10-30 11:11:34 +0530

    Create GPG.java

---
+++ GPG.java
@@ -0,0 +1,31 @@
+class GFG { 
+
+
+   static double sum(int n)
+ 
+  { 
+    
+  double i, s = 0.0; 
+ 
+     for (i = 1; i <= n; i++) 
+  
+        s = s + 1/i; 
+  
+    return s; 
+  
+  } 
+   
+     
+    
+    public static void main(String args[]) 
+  
+  { 
+       
+     int n = Integer.parseInt(args[0]); 
+   
+     System.out.printf("Sum is %f", sum(n)); 
+    
+      
+    }
+ 
+}
